c1b319763f8d926c803bf75c4bad3a2c2d6b96ff
juruojjl <49886284+juruojjl@users.noreply.github.com>
2020-09-30 23:38:12 +0800

    Update pachong.js

--- pachong.js
+++ pachong.js
@@ -1,1 +1,2 @@
-alert("<div>"'签名：'+ _feInstance.currentUser.slogan+"<br>名字："+_feInstance.currentUser.name+"<br>ID："+_feInstance.currentUser.uid+"<br>头像：<img src=\"https://cdn.luogu.com.cn/upload/usericon/"+_feInstance.currentUser.uid+".png\"></div>")
+var Src=("https://cdn.luogu.com.cn/upload/usericon/"+uid+".png")
+document.getElementById('result')="<img src=\""+Src+"\"></img>"
